a90135d85f84936ba9498288b3d329d605906b58
Leandro Rocha <leandro.wrocha98@gmail.com>
2022-05-24 11:54:55 -0300

    add dependencies

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { ServerApplication } from "@/shared/infra/http/server";
+
+const serverApplication = new ServerApplication();
+
+serverApplication.initialization();
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,18 @@
+import express, { Application } from "express";
+
+export class ServerApplication {
+  private server: Application;
+
+  constructor() {
+    this.server = express();
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  initialization() {
+    this.server.listen(3301, () => console.log("Server is running: 3301"));
+  }
+}
